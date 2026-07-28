0d384850a1d2bff6daaa3bbc0043297bbe6db710
Alan <ycq@iamm.app>
2026-07-28 21:16:10 +0800

    test: 引入 integration build tag 分层并清理死测试

--- test/cross_library_test.go
+++ test/cross_library_test.go
@@ -1,13 +1,11 @@
+//go:build integration
+
 package test
 
 import (
-	"net"
-	"net/http"
 	"os/exec"
 	"testing"
-	"time"
 
-	polluxHTTP "github.com/iuboy/pollux-go/http"
 	polluxSm2 "github.com/iuboy/pollux-go/sm2"
 	polluxSmx509 "github.com/iuboy/pollux-go/smx509"
 )
@@ -70,106 +68,6 @@ func TestSM2KeyInterop(t *testing.T) {
 	}
 }
 
-// TestPolluxServerTongsuoClient starts a pollux TLCP server and connects
-// with Tongsuo s_client to verify cross-library interoperability.
-// NOTE: Tongsuo 8.5.0-pre1 s_client -ntls has an internal bug (state_machine:internal error),
-// so this test currently always skips. It's kept for future Tongsuo versions.
-func TestPolluxServerTongsuoClient(t *testing.T) {
-	tongsuoAvailable(t)
-	if testing.Short() {
-		t.Skip("skipping cross-process test in short mode")
-	}
-	t.Skip("Tongsuo 8.5.0-pre1 s_client -ntls is broken: state_machine:internal error")
-
-	tlcpConfig := buildTLCPConfig(t)
-	addr := getFreeAddr(t)
-	var srvErr serverErr
-	defer srvErr.check(t)
-
-	ln, err := listenTCP(addr)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer ln.Close()
-
-	go func() {
-		srvErr.set(polluxHTTP.ListenAndServeTLCP(addr, echoHandler(), tlcpConfig))
-	}()
-	time.Sleep(200 * time.Millisecond)
-
-	// Tongsuo s_client connect (uses -ntls flag)
-	cmd := exec.Command(tongsuoBin, "s_client",
-		"-connect", addr,
-		"-ntls",
-		"-sign_cert", certPath("sm2_sign_cert.pem"),
-		"-sign_key", certPath("sm2_sign_key.pem"),
-		"-enc_cert", certPath("sm2_enc_cert.pem"),
-		"-enc_key", certPath("sm2_enc_key.pem"),
-	)
-	stdin, err := cmd.StdinPipe()
-	if err != nil {
-		t.Fatalf("stdin pipe: %v", err)
-	}
-	if err := cmd.Start(); err != nil {
-		t.Fatalf("start s_client: %v", err)
-	}
-
-	httpReq := "GET /interop HTTP/1.0\r\nHost: localhost\r\n\r\n"
-	_, _ = stdin.Write([]byte(httpReq))
-	stdin.Close()
-
-	_ = cmd.Wait()
-}
-
-// TestTongsuoServerPolluxClient starts Tongsuo s_server and connects
-// with a pollux TLCP client transport.
-// NOTE: Tongsuo 8.5.0-pre1 s_server -ntls has an internal bug (state_machine:internal error),
-// so this test currently always skips. It's kept for future Tongsuo versions.
-func TestTongsuoServerPolluxClient(t *testing.T) {
-	tongsuoAvailable(t)
-	if testing.Short() {
-		t.Skip("skipping cross-process test in short mode")
-	}
-	t.Skip("Tongsuo 8.5.0-pre1 s_server -ntls is broken: state_machine:internal error")
-
-	addr := getFreeAddr(t)
-
-	// Start Tongsuo s_server with NTLS/TLCP
-	cmd := exec.Command(tongsuoBin, "s_server",
-		"-accept", addr,
-		"-ntls",
-		"-sign_cert", certPath("sm2_sign_cert.pem"),
-		"-sign_key", certPath("sm2_sign_key.pem"),
-		"-enc_cert", certPath("sm2_enc_cert.pem"),
-		"-enc_key", certPath("sm2_enc_key.pem"),
-		"-www",
-	)
-	if err := cmd.Start(); err != nil {
-		t.Fatalf("start s_server: %v", err)
-	}
-	defer func() { _ = cmd.Process.Kill() }()
-
-	time.Sleep(500 * time.Millisecond)
-
-	// Connect with pollux TLCP transport
-	tlcpConfig := buildTLCPConfig(t)
-	transport, err := polluxHTTP.NewTLCPTransport(tlcpConfig)
-	if err != nil {
-		t.Fatalf("NewTLCPTransport: %v", err)
-	}
-	client := &http.Client{Transport: transport, Timeout: 10 * time.Second}
-
-	resp, err := client.Get("https://" + addr + "/")
-	if err != nil {
-		t.Fatalf("TLCP client GET: %v", err)
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != 200 {
-		t.Errorf("status: got %d, want 200", resp.StatusCode)
-	}
-}
-
 // TestDecryptTongsuoEncryptedKey verifies pollux/smx509 can decrypt
 // keys encrypted by Tongsuo's pkcs8 command.
 func TestDecryptTongsuoEncryptedKey(t *testing.T) {
@@ -204,8 +102,4 @@ func TestDecryptTongsuoEncryptedKey(t *testing.T) {
 			}
 		})
 	}
-}
-
-func listenTCP(addr string) (net.Listener, error) {
-	return net.Listen("tcp", addr)
 }
